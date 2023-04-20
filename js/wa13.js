58f7aeda6b4191628c34015bb4a1c23320f40948
AlexLLittle <alli5078@colorado.edu>
2023-04-20 11:35:09 -0600

    wa13 is done

--- js/wa13.js
+++ js/wa13.js
@@ -7,17 +7,25 @@ function NewTab() {
     window.open(
     "https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
 }
-
+// Jquerry to move button found at: https://codepen.io/ardoba/pen/ZNodRK.
+function move() {
+var changeTop = (Math.random() * ($(window).height() - $("button").height()));
+  var changeLeft = (Math.random() * ($(window).width() - $("button").width()));
+  $("button").css("margin-top", changeTop + "px");
+  $("button").css("margin-left", changeLeft + "px")
+  }
 function random() {
-    // outputInt = randomNumber(12, 79);
     Volnumber = Math.floor(Math.random() * 79);
     document.getElementById('VolText').innerHTML = `Current Volume: ${Volnumber}`
     totalClick += 1
-    if (totalClick === 5){
+    if (totalClick === 3){
         NewTab()
     }
+    if (totalClick != 1){ // so that it doesn't triggr the first time
+        move()
+    }
 }
-console.log(totalClick)  
+
 
 
 
